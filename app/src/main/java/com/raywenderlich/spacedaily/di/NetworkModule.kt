5c2a407c9ebe0ee4ea0f9cae73fdeacc69cfcc7f
Jabberi Farouk <farouk.jabberi-prestataire@laposte.fr>
2021-11-06 23:11:43 +0100

    Create logging interceptor

--- app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -1,8 +1,14 @@
 package com.raywenderlich.spacedaily.di
 
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val networkModule = module {
   single(named("BASE_URL")){ "https://api.nasa.gov/plantary/"}
+  single {
+    val interceptor=HttpLoggingInterceptor()
+    interceptor.level=HttpLoggingInterceptor.Level.BODY
+    interceptor
+  }
 }
