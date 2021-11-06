b5f1af53d0bf3572494c6cf8950042b7b7f4e092
Jabberi Farouk <farouk.jabberi-prestataire@laposte.fr>
2021-11-06 23:25:44 +0100

    Create the Moshi instance

--- app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.raywenderlich.spacedaily.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.BuildConfig
@@ -8,19 +10,24 @@ import org.koin.dsl.module
 import java.util.concurrent.TimeUnit.SECONDS
 
 val networkModule = module {
-  single(named("BASE_URL")){ "https://api.nasa.gov/plantary/"}
+  single(named("BASE_URL")) { "https://api.nasa.gov/plantary/" }
   single {
-    val interceptor=HttpLoggingInterceptor()
-    interceptor.level=HttpLoggingInterceptor.Level.BODY
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
     interceptor
   }
   single {
-    val client=OkHttpClient().newBuilder()
-      .connectTimeout(30,SECONDS)
-      .writeTimeout(30,SECONDS)
-      .readTimeout(30,SECONDS)
-    if(BuildConfig.DEBUG)
+    val client = OkHttpClient().newBuilder()
+      .connectTimeout(30, SECONDS)
+      .writeTimeout(30, SECONDS)
+      .readTimeout(30, SECONDS)
+    if (BuildConfig.DEBUG)
       client.addInterceptor(get<HttpLoggingInterceptor>())
     client.build()
+  }
+  single {
+    Moshi.Builder()
+      .add(KotlinJsonAdapterFactory()).build()
+
   }
 }
