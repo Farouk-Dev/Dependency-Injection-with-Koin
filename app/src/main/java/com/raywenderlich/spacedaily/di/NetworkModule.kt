2d5a6afda602bcd73deab3720cdc60396c204d66
Jabberi Farouk <farouk.jabberi-prestataire@laposte.fr>
2021-11-06 23:19:59 +0100

    Create the okhttpClient

--- app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -1,8 +1,11 @@
 package com.raywenderlich.spacedaily.di
 
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.BuildConfig
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import java.util.concurrent.TimeUnit.SECONDS
 
 val networkModule = module {
   single(named("BASE_URL")){ "https://api.nasa.gov/plantary/"}
@@ -10,5 +13,14 @@ val networkModule = module {
     val interceptor=HttpLoggingInterceptor()
     interceptor.level=HttpLoggingInterceptor.Level.BODY
     interceptor
+  }
+  single {
+    val client=OkHttpClient().newBuilder()
+      .connectTimeout(30,SECONDS)
+      .writeTimeout(30,SECONDS)
+      .readTimeout(30,SECONDS)
+    if(BuildConfig.DEBUG)
+      client.addInterceptor(get<HttpLoggingInterceptor>())
+    client.build()
   }
 }
